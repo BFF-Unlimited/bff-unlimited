02874f0efa02d6f3d9c277575ac12baebfa75a77
Wouter Huizinga <wouter.huizinga@rovict.nl>
2022-09-12 12:54:26 +0200

    BFF-15 Move project from backend to server

--- Backend/Bff.WebApi/UserStore/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bff.WebApi.UserStore
-{
-    public class UserDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
