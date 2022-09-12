c1ecc4da7ddb2cfc9f887258bf9e4a9274dfccd1
Wouter Huizinga <wouter.huizinga@rovict.nl>
2022-09-12 08:43:49 +0200

    BFF-15 Create custom login with users Ans, Marlies, Willem

---
+++ Backend/Bff.WebApi/App_Start/AuthConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using System.Globalization;
+using Ninject.Activation;
+using Bff.WebApi.Managers;
+using Bff.WebApi.Handlers;
+
+namespace Bff.WebApi
+{ 
+    public static class AuthConfig
+    {
+        public static void UseAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication("Basic")
+                .AddScheme<BasicAuthenticationOptions, CustomAuthenticationHandler>("Basic", null);
+
+            builder.Services.AddSingleton<ICustomAuthenticationManager, CustomAuthenticationManager>();
+        }
+
+    }
+}
---
+++ Backend/Bff.WebApi/Handlers/CustomAuthenticationHandler.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using Bff.WebApi.Managers;
+
+namespace Bff.WebApi.Handlers
+{
+    public class CustomAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions>
+    {
+        private readonly ICustomAuthenticationManager customAuthenticationManager;
+
+        public CustomAuthenticationHandler(
+            IOptionsMonitor<BasicAuthenticationOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            ICustomAuthenticationManager customAuthenticationManager)
+            : base(options, logger, encoder, clock)
+        {
+            this.customAuthenticationManager = customAuthenticationManager;
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Unauthorized");
+
+            string authorizationHeader = Request.Headers["Authorization"];
+            if (string.IsNullOrEmpty(authorizationHeader))
+            {
+                return AuthenticateResult.NoResult();
+            }
+
+            if (!authorizationHeader.StartsWith("bearer", StringComparison.OrdinalIgnoreCase))
+            {
+                return AuthenticateResult.Fail("Unauthorized");
+            }
+
+            string token = authorizationHeader.Substring("bearer".Length).Trim();
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return AuthenticateResult.Fail("Unauthorized");
+            }
+
+            try
+            {
+                return validateToken(token);
+            }
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail(ex.Message);
+            }
+        }
+
+        private AuthenticateResult validateToken(string token)
+        {
+            var validatedToken = customAuthenticationManager.Tokens.FirstOrDefault(t => t.Key == token);
+            if (validatedToken.Key == null)
+            {
+                return AuthenticateResult.Fail("Unauthorized");
+            }
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, validatedToken.Value),
+                };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new System.Security.Principal.GenericPrincipal(identity, null);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+}
