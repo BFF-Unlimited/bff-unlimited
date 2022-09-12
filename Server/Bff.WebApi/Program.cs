e9b9f8ebbad8b5d89237768cd40a7926785e6412
Wouter Huizinga <wouter.huizinga@rovict.nl>
2022-09-12 12:54:26 +0200

    BFF-15 Move project from backend to server

--- Server/Bff.WebApi/Program.cs
+++ Server/Bff.WebApi/Program.cs
@@ -12,7 +12,7 @@ namespace Bff.WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+             
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
