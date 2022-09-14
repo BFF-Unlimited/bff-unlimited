9f507f83e2335e8e953ce5c6c451be5f7565bbcf
Rahmat Syamsuddin <rahmat.syamsuddin@rovict.nl>
2022-09-14 12:47:37 +0200

    verbose logging weggehaald.

--- Start_docker_setup.bat
+++ Start_docker_setup.bat
@@ -1,3 +1,3 @@
-dotnet dev-certs https --trust -v -ep %USERPROFILE%\.aspnet\https\Bff.WebApi.pfx -p bff123
+dotnet dev-certs https --trust -ep %USERPROFILE%\.aspnet\https\Bff.WebApi.pfx -p bff123
 
 docker-compose up -d
