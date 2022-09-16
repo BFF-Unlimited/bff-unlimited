7995fc7f5543e10f3b24375ec65f445e35388921
Raymon S <raymon@raymonschouwenaar.nl>
2022-09-16 16:34:43 +0200

    Add menu

--- Client/src/components/app-sidebar/AppSidebar.vue
+++ Client/src/components/app-sidebar/AppSidebar.vue
@@ -1,25 +1,65 @@
 <template>
-    <aside>
-        Sidebar
-    </aside>
-  </template>
-  
-  <script setup>
-  defineProps({
-    type: {
-      type: String,
-      default: 'button',
-    },
-    label: {
-      type: String,
-      default: 'Opslaan',
-    },
-    primaryAction: {
-      type: Boolean,
-      default: false,
-    },
-  });
-  </script>
-  
-  <style src="./app-sidebar.css" scoped lang="postcss" />
-  
+  <div class="side-nav">
+    <ul class="side-nav-ul">
+      <li
+        v-for="item of menu"
+        :key="item?.text"
+        v-bind:class="{ active: true }"
+      >
+        <NuxtLink :to="item?.link">{{ item?.text }}</NuxtLink>
+        <ul
+          class="side-sub-ul"
+          v-if="item?.submenu"
+        >
+          <li
+            v-for="sub of item.submenu"
+            :key="sub?.text"
+          >
+            <NuxtLink :to="sub?.link">{{ sub?.text }}</NuxtLink>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  menu: {
+    type: String,
+    default: 'button',
+  },
+});
+</script>
+
+<script>
+const menu = [
+  {
+    text: 'Groep 4b',
+    link: '/',
+    submenu: [
+      {
+        text: 'Overzicht',
+        link: '/',
+      },
+      {
+        text: 'Groepsplan',
+        link: '/',
+      },
+      {
+        text: 'Over de groep',
+        link: '/',
+      },
+    ],
+  },
+];
+export default {
+  data() {
+    return {
+      menu,
+    };
+  },
+};
+</script>
+
+<style src="./app-sidebar.css" scoped lang="postcss" />
--- Client/src/pages/index.vue
+++ Client/src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="main">
     <template #header>Header</template>
-    <template #sidebar>Sidebar</template>
+    <template #sidebar><AppSidebar /></template>
     <template #default><h1>Hello World</h1></template>
   </NuxtLayout>
 </template>
