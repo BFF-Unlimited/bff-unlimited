d9e4b3f9f96a45fc759dce43dd59f21716f88aec
Gino <gino.bolt@rovict.nl>
2022-09-20 11:01:06 +0200

    UserController hernoemd naar LoginController.
    UserDto hernoemd naar LoginDto.

--- Server/Bff.WebApi/UserStore/LoginDto.cs
+++ Server/Bff.WebApi/UserStore/LoginDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Bff.WebApi.UserStore
 {
-    public class UserDto
+    public class LoginDto
     {
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
