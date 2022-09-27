23e5ccc3056f987e75b53db10335041c3bf6fca7
Rahmat Syamsuddin <rahmat.syamsuddin@rovict.nl>
2022-09-27 11:09:02 +0200

    Insert script Users aangemaakt

---
+++ Server/Bff.WebApi.Administrations/DataAccess/Mysql/UsersInsert.sql
@@ -0,0 +1,11 @@
+insert into Users (Username, Password)
+values ('Rahmat', '1234'),
+('Wouter', '1234'),
+('Gino', '1234'),
+('Sanne', '1234'),
+('Raymon', '1234'),
+('Ans', '1234'),
+('Marlies', '1234'),
+('Willem', '1234'),
+('test1', '1234'),
+('test12', '1234')
