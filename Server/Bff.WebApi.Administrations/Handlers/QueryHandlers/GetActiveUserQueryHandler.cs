8d6e4971f3fc650e421754192b464f3d1099d125
Wouter Huizinga <wouter.huizinga@rovict.nl>
2022-10-06 08:40:54 +0200

    ESIS-95 Remove some array's in the DTO's

--- Server/Bff.WebApi.Administrations/Handlers/QueryHandlers/GetActiveUserQueryHandler.cs
+++ Server/Bff.WebApi.Administrations/Handlers/QueryHandlers/GetActiveUserQueryHandler.cs
@@ -10,23 +10,23 @@ namespace Bff.WebApi.Services.Administrations.Handlers.QueryHandlers
         {
             var vestiging = new VestigingIdentificationDto("logoUrl", "Vestiging");
             var groep = new GroepIdentificationDto("Groep");
-
-            return await Task.FromResult(new UserDto(
-                userName: "Ans",
-                activeVestiging: vestiging,
-                activeGroep: groep,
-                vestigingen: new[] { vestiging },
-                groepen: new[] { groep },
-                permissions: new[] {
-                    new PermissionDto("permission://Groep.Overview", "Groep.Overview", "Groepsoverzicht"),
-                    new PermissionDto("permission://Groep.Groepsplan", "Groep.Groepsplan", "Groepsplan"),
-                    new PermissionDto("permission://Groep.About", "Groep.About", "Over"),
-                    new PermissionDto("permission://Leerling.Overview", "Leerling.Overview", "Leerlingenoverzicht"),
-                    new PermissionDto("permission://Leerling.Search", "Leerling.Search", "Leerling zoeken"),
-                    new PermissionDto("permission://Registration.Overview", "Registration.Overview", "Registratieoverzicht"),
-                    new PermissionDto("permission://Registration.Create", "Registration.Create", "Registratie aanmaken")
-                }));
-            ;
+            var result = new UserDto
+            {
+                UserName = "Ans",
+                ActiveVestiging = vestiging,
+                ActiveGroep = groep
+            };
+            result.Vestigingen.Add(vestiging);
+            result.Groepen.Add(groep);
+            result.Permissions.Add(new PermissionDto("permission://Groep.Overview", "Groep.Overview", "Groepsoverzicht"));
+            result.Permissions.Add(new PermissionDto("permission://Groep.Groepsplan", "Groep.Groepsplan", "Groepsplan"));
+            result.Permissions.Add(new PermissionDto("permission://Groep.About", "Groep.About", "Over"));
+            result.Permissions.Add(new PermissionDto("permission://Leerling.Overview", "Leerling.Overview", "Leerlingenoverzicht"));
+            result.Permissions.Add(new PermissionDto("permission://Leerling.Search", "Leerling.Search", "Leerling zoeken"));
+            result.Permissions.Add(new PermissionDto("permission://Registration.Overview", "Registration.Overview", "Registratieoverzicht"));
+            result.Permissions.Add(new PermissionDto("permission://Registration.Create", "Registration.Create", "Registratie aanmaken"));
+            
+            return await Task.FromResult(result);
         }
     }
 }
--- Server/Bff.WebApi.Administrations/Handlers/QueryHandlers/GetCardsDataQueryHandler.cs
+++ Server/Bff.WebApi.Administrations/Handlers/QueryHandlers/GetCardsDataQueryHandler.cs
@@ -6,18 +6,17 @@ namespace Bff.WebApi.Services.Administrations.Handlers.QueryHandlers
 {
     internal class GetCardsDataQueryHandler : AsyncQueryHandlerBase<GetCardsDataQuery>
     {
-        protected override async Task<object> DoExecute(GetCardsDataQuery query) =>
-            await Task.FromResult(() =>
-            {
-                var result = new CardsDataDto();
-                result.Absenties.Add(new AbsentieDto { LeerlingId = Guid.NewGuid(), StartDate = new DateTime(2022, 9, 28), EndDate = new DateTime(2022, 9, 28), Reason = "Jantje is gisteren keihard van het klimrek gevallen" });
-                result.JarigeLeerlingen.Add(new LeerlingDto { FirstName = "Jantje", LastName = "Jansen", DateOfBirth = DateTime.Now });
-                result.Koppelingen.Add(new KoppelingDto { Name = "Momento", Url = "https://www.momento.nl", LogoUrl = "https://www.momento.nl/logo" });
-                result.LaatstBekekenPaginas.Add(new LaatstBekekenPaginaDto { Id = "permission://Groep.Overview", CategoryDescription = "Groep.Overview", Description = "Groepsoverzicht" });
-                result.LaatstBekekenPaginas.Add(new LaatstBekekenPaginaDto { Id = "permission://Leerling.Overview", CategoryDescription = "Leerling.Overview", Description = "Leerlingenoverzicht" });
-                result.Notities.Add(new NotitieDto { LeerlingId = Guid.NewGuid(), DateOfOccurrence = DateTime.Now, Description = "Jantje heeft de juffrouw geslagen" });
+        protected override async Task<object> DoExecute(GetCardsDataQuery query)
+        {
+            var result = new CardsDataDto();
+            result.Absenties.Add(new AbsentieDto { LeerlingId = Guid.NewGuid(), StartDate = new DateTime(2022, 9, 28), EndDate = new DateTime(2022, 9, 28), Reason = "Jantje is gisteren keihard van het klimrek gevallen" });
+            result.JarigeLeerlingen.Add(new LeerlingDto { FirstName = "Jantje", LastName = "Jansen", DateOfBirth = DateTime.Now });
+            result.Koppelingen.Add(new KoppelingDto { Name = "Momento", Url = "https://www.momento.nl", LogoUrl = "https://www.momento.nl/logo" });
+            result.LaatstBekekenPaginas.Add(new LaatstBekekenPaginaDto { Id = "permission://Groep.Overview", CategoryDescription = "Groep.Overview", Description = "Groepsoverzicht" });
+            result.LaatstBekekenPaginas.Add(new LaatstBekekenPaginaDto { Id = "permission://Leerling.Overview", CategoryDescription = "Leerling.Overview", Description = "Leerlingenoverzicht" });
+            result.Notities.Add(new NotitieDto { LeerlingId = Guid.NewGuid(), DateOfOccurrence = DateTime.Now, Description = "Jantje heeft de juffrouw geslagen" });
 
-                return result;
-            });
+            return await Task.FromResult(result);
+        }
     }
 }
