1386f9a91dedd03982a7f996f58f321e96fb4062
Marco de Wild <marco.dewild@rovict.nl>
2022-10-06 09:34:44 +0200

    Uitgecommend appform weggehaald

--- Client/src/pages/login.vue
+++ Client/src/pages/login.vue
@@ -6,33 +6,6 @@
           <h2>Inloggen</h2>
         </template>
         <template #body>
-          <!--<AppForm
-            action="/token"
-            method="post"
-            button-label="Inloggen"
-            has-primary-button
-            @validated="onValidated"
-            @success="onSuccess"
-          >
-            <FormInput
-              v-model="username"
-              type="text"
-              name="username"
-              label="Gebruikersnaam"
-              :should-validate="shouldValidate"
-              validation-error-message="Gebruikersnaam is verplicht"
-              required
-            />
-            <FormInput
-              v-model="password"
-              type="text"
-              name="password"
-              label="Password"
-              :should-validate="shouldValidate"
-              validation-error-message="Wachtwoord is verplicht"
-              required
-            />
-          </AppForm>-->
           <AppButton v-on:click="redirectToLogin()" 
             :label="'Inloggen'"></AppButton>
         </template>
